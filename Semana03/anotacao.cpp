9552ac7945ed69f5722c3ea5620ec646714de9b8
Gabriel Matheus <gabrielmatheusdesouza26@gmail.com>
2024-04-19 20:28:44 -0300

    ajustes

---
+++ Semana03/anotacao.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    /*
+    
+     ANOTAÇÕES 16/04/2024
+
+     Customização em linux: UnixPorn (reddit)
+     Instalar: Zorin
+    
+     Revisão de vector
+
+     vector<int> inteiros != int inteiros[n]
+     vetor tem tamanho fixo.
+
+     funções de vector: 
+     .push_back(element);
+     .erase(element);
+     .clear();
+
+    -- cin lê até o primeiro espaço vazio ou até o enter
+
+    -- matrizes:
+        array de arrays
+        ex.: M[3][3] = M(3x3)
+
+        int matriz[3][3]
+
+        percorrendo a matriz e preenchendo:
+        for(int linha = 0; linha < 3; linha++)
+        {
+            for(int coluna = 0; coluna < 3; coluna++)
+            {
+                int num;
+                cin >> num;
+                matriz[linha][coluna] = num;
+            }
+        }   
+
+        --Funções:
+        int soma(int a, int b)
+        {
+            int soma = a + b;
+            return soma;
+        }
+    */
+    return 0;
+}
